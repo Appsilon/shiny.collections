d4ff40e9e51d232cb8dcf0687b8e4986c0ea0a68
Dominik Krzeminski (dokato) <raymon92@gmail.com>
2017-04-11 16:06:34 +0200

    fixed messages order

--- inst/examples/chat.R
+++ inst/examples/chat.R
@@ -11,6 +11,10 @@ connection <- shiny.collections::connect()
 
 chat_printing <- function(tibble_df){
   stroutput <- ""
+  if (is_empty(colnames(tibble_df))){
+    return(stroutput)
+  }
+  tibble_df <- tibble_df[order(tibble_df$time),]
   for (i in 1:dim(tibble_df)[[1]]) {
     newrow <- paste(tibble_df$user[i], " : ", tibble_df$text[i])
     stroutput <- paste(stroutput, newrow, '\n')
@@ -21,7 +25,7 @@ chat_printing <- function(tibble_df){
 server <- shinyServer(function(input, output, session) {
   chat <- shiny.collections::collection("chat", connection)
   observeEvent(input$send, {
-    shiny.collections::insert(chat, list(user = "User1", text = input$msgText))
+    shiny.collections::insert(chat, list(user = "User1", text = input$msgText, time = Sys.time()))
     print(chat_printing(chat$collection))
     updateTextInput(session, "msgText", value = "")
   })
@@ -30,8 +34,6 @@ server <- shinyServer(function(input, output, session) {
     updateTextAreaInput(session, "caption", value = chat_printing(chat$collection))
   })
 
-
-  #print(DT::datatable(chat$collection))
 })
 
 shinyApp(ui = ui, server = server)
