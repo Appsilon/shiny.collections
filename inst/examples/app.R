0ee88dd81e92669a15cb4187cc529b1d7eca1e73
Dominik Krzeminski (dokato) <raymon92@gmail.com>
2017-04-11 12:36:43 +0200

    bug #1 in exampleapp fixed

--- inst/examples/app.R
+++ inst/examples/app.R
@@ -11,7 +11,7 @@ server <- shinyServer(function(input, output) {
   cars <- shiny.collections::collection("cars", connection)
 
   observeEvent(input$click, {
-    shiny.collections::insert("cars", list(name = "Sample name", value = sample(1:100, 1)), connection)
+    shiny.collections::insert(cars, list(name = "Sample name", value = sample(1:100, 1)))
   })
   output$cars_data <- DT::renderDataTable(DT::datatable(cars$collection))
 })
