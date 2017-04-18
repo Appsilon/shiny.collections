553185ce246df0ab2d1c7aa24bd819878ef2f669
Dominik Krzeminski (dokato) <raymon92@gmail.com>
2017-04-18 10:29:19 +0200

    chat extended with javascript features

--- inst/examples/chat.R
+++ inst/examples/chat.R
@@ -1,9 +1,38 @@
 library(shiny)
+library(purrr)
+library(dplyr)
+library(magrittr)
 
 ui <- shinyUI(fluidPage(
+  tags$head(
+    tags$style(HTML("
+                    #chatbox {
+                     padding: .5em;
+                     border: 1px solid #777;
+                     height: 300px;
+                     overflow-y: scroll;
+                    }
+                    "))
+  ),
+  #' maybe possible easier ?
+  tags$script("
+              var oldContent = null;
+              window.setInterval(function() {
+              var elem = document.getElementById('chatbox');
+              if (oldContent != elem.innerHTML){
+              scrollToBottom();
+              }
+              oldContent = elem.innerHTML;
+              }, 300);
+              function scrollToBottom(){
+              var elem = document.getElementById('chatbox');
+              elem.scrollTop = elem.scrollHeight;
+              }
+              "),
   titlePanel("Chat app (shiny.collections demo)"),
-  textAreaInput("caption", "Message Box", width = "600px", height = "300px"),
-  div(style="display:inline-block", textInput("msgText", "Your message", width='400px')),
+  div(style="display:inline-block", textInput("unText", "Username", width='200px')),
+  uiOutput("chatbox"),
+  div(style="display:inline-block", textInput("msgText", "Your message", width='500px')),
   div(style="display:inline-block", actionButton("send", "Send"))
 ))
 
@@ -22,18 +51,35 @@ chat_printing <- function(tibble_df){
   return(stroutput)
 }
 
+#' TODO - collect only N latest
+render_msg_divs <- function(collection){
+    div(class="ui very relaxed list",
+        collection %>% arrange(time) %>%
+          by_row(~ div(class="item",
+                      a(class="header", .$user),
+                      div(class="description", .$text)
+                      )
+                ) %>% {.$.out}
+       )
+}
+
 server <- shinyServer(function(input, output, session) {
   chat <- shiny.collections::collection("chat", connection)
+  #' default username is a big int number
+  updateTextInput(session, "unText", value = paste0("User", round(runif(1, 10000, 99999))))
   observeEvent(input$send, {
-    shiny.collections::insert(chat, list(user = "User1", text = input$msgText, time = Sys.time()))
-    print(chat_printing(chat$collection))
+    shiny.collections::insert(chat, list(user = input$unText, text = input$msgText, time = Sys.time()))
     updateTextInput(session, "msgText", value = "")
   })
   observeEvent(chat$collection, {
-    #output$caption <- chat_printing(chat$collection)
-    updateTextAreaInput(session, "caption", value = chat_printing(chat$collection))
+    output$chatbox <- renderUI({
+      tryCatch({
+        render_msg_divs(chat$collection)
+      }, error = function(err) {
+        tags$span("Empty chat")
+      })
+    })
   })
-
 })
 
 shinyApp(ui = ui, server = server)
