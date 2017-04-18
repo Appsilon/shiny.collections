05c7fb4466f67a7eb893ebf5034c751fbc774815
Dominik Krzeminski (dokato) <raymon92@gmail.com>
2017-04-18 10:40:02 +0200

    coding style amendments according to lintr

--- inst/examples/chat.R
+++ inst/examples/chat.R
@@ -30,34 +30,24 @@ ui <- shinyUI(fluidPage(
               }
               "),
   titlePanel("Chat app (shiny.collections demo)"),
-  div(style="display:inline-block", textInput("unText", "Username", width='200px')),
+  div(style = "display:inline-block",
+      textInput("unText", "Username", width = "200px")),
   uiOutput("chatbox"),
-  div(style="display:inline-block", textInput("msgText", "Your message", width='500px')),
-  div(style="display:inline-block", actionButton("send", "Send"))
+  div(style = "display:inline-block",
+      textInput("msgText", "Your message", width = "500px")),
+  div(style = "display:inline-block",
+      actionButton("send", "Send"))
 ))
 
 connection <- shiny.collections::connect()
 
-chat_printing <- function(tibble_df){
-  stroutput <- ""
-  if (is_empty(colnames(tibble_df))){
-    return(stroutput)
-  }
-  tibble_df <- tibble_df[order(tibble_df$time),]
-  for (i in 1:dim(tibble_df)[[1]]) {
-    newrow <- paste(tibble_df$user[i], " : ", tibble_df$text[i])
-    stroutput <- paste(stroutput, newrow, '\n')
-  }
-  return(stroutput)
-}
-
 #' TODO - collect only N latest
 render_msg_divs <- function(collection){
-    div(class="ui very relaxed list",
+    div(class = "ui very relaxed list",
         collection %>% arrange(time) %>%
-          by_row(~ div(class="item",
-                      a(class="header", .$user),
-                      div(class="description", .$text)
+          by_row(~ div(class = "item",
+                      a(class = "header", .$user),
+                      div(class = "description", .$text)
                       )
                 ) %>% {.$.out}
        )
@@ -66,9 +56,13 @@ render_msg_divs <- function(collection){
 server <- shinyServer(function(input, output, session) {
   chat <- shiny.collections::collection("chat", connection)
   #' default username is a big int number
-  updateTextInput(session, "unText", value = paste0("User", round(runif(1, 10000, 99999))))
+  updateTextInput(session, "unText",
+                  value = paste0("User", round(runif(1, 10000, 99999))))
   observeEvent(input$send, {
-    shiny.collections::insert(chat, list(user = input$unText, text = input$msgText, time = Sys.time()))
+    shiny.collections::insert(chat,
+                              list(user = input$unText,
+                                   text = input$msgText,
+                                   time = Sys.time()))
     updateTextInput(session, "msgText", value = "")
   })
   observeEvent(chat$collection, {
