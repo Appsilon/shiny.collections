e565d22e889f324dfdc8fa89c9d8542f613413b3
Dominik Krzeminski <raymon92@gmail.com>
2017-04-28 12:03:10 +0200

    suggested amendments introduced

--- inst/examples/chat.R
+++ inst/examples/chat.R
@@ -1,79 +1,91 @@
+#' Shiny Chat example using shiny.collections
+
 library(shiny)
 library(purrr)
 library(dplyr)
 library(magrittr)
 
+#' This global variable describes how many recent variables are visible
+#' in the chat window.
+VISIBLE_MESSAGES <- 40
+
+# Helper Functions
+##############################
+
+#' Render divs with messages
+#'
+#' @param collection Collection of entries from rethinkDB
+#'
+#' @return div object with formetted entries
+render_msg_divs <- function(collection){
+  div(class = "ui very relaxed list",
+      collection %>%
+        arrange(time) %>%
+        tail(VISIBLE_MESSAGES) %>%
+        by_row(~ div(class = "item",
+                     a(class = "header", .$user),
+                     div(class = "description", .$text)
+        )
+        ) %>% {.$.out}
+  )
+}
+
+#' Get random username
+#'
+#' @return Character "User"+random number 10000<x99999
+get_random_username <- function(){
+  paste0("User", round(runif(1, 10000, 99999)))
+}
+
+# User Interface of the Chat
+##############################
+
 ui <- shinyUI(fluidPage(
   tags$head(
-    tags$style(HTML("
-                    #chatbox {
-                     padding: .5em;
-                     border: 1px solid #777;
-                     height: 300px;
-                     overflow-y: scroll;
-                    }
-                    "))
+    tags$link(rel = "stylesheet", type = "text/css", href = "style.css"),
+    tags$script(src = "script.js")
   ),
-  #' maybe possible easier ?
-  tags$script("
-              var oldContent = null;
-              window.setInterval(function() {
-              var elem = document.getElementById('chatbox');
-              if (oldContent != elem.innerHTML){
-              scrollToBottom();
-              }
-              oldContent = elem.innerHTML;
-              }, 300);
-              function scrollToBottom(){
-              var elem = document.getElementById('chatbox');
-              elem.scrollTop = elem.scrollHeight;
-              }
-              "),
   titlePanel("Chat app (shiny.collections demo)"),
-  div(style = "display:inline-block",
-      textInput("unText", "Username", width = "200px")),
+  div(textInput("username_field", "Username", width = "200px")),
   uiOutput("chatbox"),
   div(style = "display:inline-block",
-      textInput("msgText", "Your message", width = "500px")),
+      textInput("message_field", "Your message", width = "500px")),
   div(style = "display:inline-block",
       actionButton("send", "Send"))
 ))
 
-connection <- shiny.collections::connect()
+# Server with app logic
+##############################
 
-#' TODO - collect only N latest
-render_msg_divs <- function(collection){
-    div(class = "ui very relaxed list",
-        collection %>% arrange(time) %>%
-          by_row(~ div(class = "item",
-                      a(class = "header", .$user),
-                      div(class = "description", .$text)
-                      )
-                ) %>% {.$.out}
-       )
-}
+#' Before running the server we set up a connection with rethinkDB
+connection <- shiny.collections::connect()
 
 server <- shinyServer(function(input, output, session) {
   chat <- shiny.collections::collection("chat", connection)
-  #' default username is a big int number
-  updateTextInput(session, "unText",
-                  value = paste0("User", round(runif(1, 10000, 99999))))
+  #' By default the username is a big int number.
+  updateTextInput(session, "username_field",
+                  value = get_random_username()
+                  )
   observeEvent(input$send, {
-    shiny.collections::insert(chat,
-                              list(user = input$unText,
-                                   text = input$msgText,
-                                   time = Sys.time()))
-    updateTextInput(session, "msgText", value = "")
+    new_message <- list(user = input$username_field,
+                        text = input$message_field,
+                        time = Sys.time())
+    shiny.collections::insert(chat, new_message)
+    updateTextInput(session, "message_field", value = "")
   })
+
   observeEvent(chat$collection, {
     output$chatbox <- renderUI({
-      tryCatch({
+      #' We render message boxes (divs) only if collections
+      #' contains some entries.
+      if (!is_empty(chat$collection)) {
         render_msg_divs(chat$collection)
-      }, error = function(err) {
+      } else {
         tags$span("Empty chat")
-      })
+      }
     })
   })
+
 })
 
 shinyApp(ui = ui, server = server)
