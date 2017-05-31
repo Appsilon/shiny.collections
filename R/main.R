6b54c2a0a19d38b6e48fdf36959905574cc2c57d
Dominik Krzeminski <raymon92@gmail.com>
2017-05-31 12:15:36 +0200

    consts exported to config.R and some functions documented

--- R/main.R
+++ R/main.R
@@ -1,23 +1,66 @@
-DEFAULT_CONFIG_NAME <- "rethinkdb_config.yml"
-DEFAULT_DB <- "shiny"
-
 .onAttach <- function(...) {
   # assign("cached_connection", NULL, .GlobalEnv)
 }
 
+#' Make sure that rethink database exists
+#'
+#' This function checks whether database `db_name` exists and if not
+#' it creates one.
+#'
+#' @param connection structure with rethinkdb connection details
+#' @param db_name character with database name
+#' @export
+#'
+#' @examples
+#' cn <- connect()
+#' make_sure_db_exists(cn, "temp_db")
 make_sure_db_exists <- function(connection, db_name) {
   if (!(db_name %in% rethinker::r()$dbList()$run(connection$raw_connection))) {
     rethinker::r()$dbCreate(db_name)$run(connection$raw_connection)
   }
 }
 
-make_sure_table_exists <- function(connection, table_name) {
-  if (!(table_name %in% rethinker::r()$db(DEFAULT_DB)$tableList()$run(connection$raw_connection))) {
-    rethinker::r()$db(DEFAULT_DB)$tableCreate(table_name)$run(connection$raw_connection)
+#' Make sure that rethinkdb table exists
+#'
+#' This function checks whether table `table_name` exists in databse `db_name`
+#' and if not it creates one.
+#'
+#' @param connection structure with rethinkdb connection details
+#' @param table_name character with table name
+#' @param db_name character with databse name (default `DEFAULT_DB`)
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' cn <- connect()
+#' make_sure_db_exists(cn, "temp_db")
+#' make_sure_table_exists(cn, "table one", temp_db")
+make_sure_table_exists <- function(connection, table_name, db_name = DEFAULT_DB) {
+  if (!(table_name %in% rethinker::r()$db(db_name)$tableList()$run(connection$raw_connection))) {
+    rethinker::r()$db(db_name)$tableCreate(table_name)$run(connection$raw_connection)
   }
 }
 
-connect <- function(host = "localhost", port = "28015", config_file = DEFAULT_CONFIG_NAME) {
+#' Connect with RethinkDB
+#'
+#' This function reads config file (if exists) or from `host` and `port`
+#' from parameters. Next, it opens a connection and creates database `db_name`
+#' (only if  already does not exists).
+#'
+#' @param host character with host address (default `localhost`)
+#' @param port character with port number (default `28015`)
+#' @param db_name character with databse name (default `DEFAULT_DB`)
+#' @param config_file character with rethink configuration file name
+#' (default `DEFAULT_CONFIG_NAME`)
+#'
+#' @return connection structure
+#' @export
+#'
+#' @examples
+#' cn <- connect()
+connect <- function(host = "localhost", port = "28015", db_name = DEFAULT_DB,
+                    config_file = DEFAULT_CONFIG_NAME) {
   if (file.exists(config_file)) {
     config <- yaml::yaml.load_file(config_file)
   } else {
@@ -29,16 +72,31 @@ connect <- function(host = "localhost", port = "28015", config_file = DEFAULT_CO
     config = config,
     raw_connection = rethinker::openConnection(config$host, config$port)
   )
-  make_sure_db_exists(connection, DEFAULT_DB)
+  make_sure_db_exists(connection, db_name)
   connection
 }
 
+#' Clone connection
+#'
+#' @param connection structure with rethinkdb connection details
+#'
+#' @return copy of connection structure
+#' @export
+#'
+#' @examples
+#' cn <- connect()
+#' cn_copy <- clone_connection(cn)
 clone_connection <- function(connection) {
   host <- connection$config$host
   port <- connection$config$port
   connect(host = host, port = port)
 }
 
+#' Drain RethinkDB connection
+#'
+#' @param connection structure with rethinkdb connection details
+#'
+#' @export
 clear <- function(connection) {
   rethinker::drainConnection(connection$raw_connection)
 }
@@ -67,7 +125,6 @@ cursor_to_tibble <- function(cursor, column_names) {
 #' @export
 #'
 #' @examples
-#'
 #' # Set up a connection to DEFAULT_DB
 #' cn <- connect()
 #' tvs <- collection("tv_shows", cn)
