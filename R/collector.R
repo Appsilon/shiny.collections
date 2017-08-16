38a83fee64a391170263e69fb0060665a53c8ab8
Dominik Krzeminski (dokato) <raymon92@gmail.com>
2017-08-17 00:12:49 +0200

    collector reference class refactoring

--- R/collector.R
+++ R/collector.R
@@ -1,31 +1,40 @@
+setOldClass("reactivevalues")
+
+#' Collector
+#'
+#' @field all reactive value with collection values.
+#' @field name character with a name of collection.
+#' @field connection list with connection details
+#'
+#' @export
+#'
+#' @rdname collector
+#'
+#' @examples
+#' #TODO
 Collector <- setRefClass("Collector",
                          fields = list(all = "reactivevalues",
-                                       collection = "tbl_df",
                                        name = "character",
                                        connection = "list")
 )
 
-setOldClass("reactivevalues")
-
+#' @rdname collector
 Collector$methods(
   initialize = function(collection_name, connection) {
     make_sure_table_exists(connection, collection_name)
-    all <<- collect(collection_name, connection)
-    collection <<- all$collection
+    all <<- collection(collection_name, connection)
     connection <<- connection
     name <<- collection_name
   },
   query = function(column_names = character(), post_process = I){
-    collect(collection_name, connection, column_names, post_process)
+    collection(collection_name, connection, column_names, post_process)
   },
   insert = function(element, ...) {
     insert(all, element, ...)
-    all <<- collect(collection_name, connection)
-    collection <<- all$collection
+    all <<- collection(collection_name, connection)
   },
   delete = function(element_id) {
     delete(all, element_id)
-    all <<- collect(collection_name, connection)
-    collection <<- all$collection
+    all <<- collection(collection_name, connection)
   }
 )
